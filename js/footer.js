99b4e798ab4388689d605f28b940c1c5acf10d86
Link <2042183586@qq.com>
2023-09-20 22:59:02 +0800

    Site updated: 2023-09-20 22:58:54

---
+++ js/footer.js
@@ -0,0 +1,28 @@
+// 动态心跳
+$(document).ready(function(e){
+    $('.copyright').html('©2023 - 2023 <i class="fa-fw fas fa-heartbeat fa-beat" style="color: #f92672;"></i> By Link');
+})
+
+$(document).ready(function(e){
+    show_date_time();
+})
+
+//本站运行时间
+function show_date_time(){
+$('.framework-info').html('本站已安全运行<span id="span_dt_dt" style="color: #fff;"></span>');
+window.setTimeout("show_date_time()", 1000);
+BirthDay=new Date("2/1/2023 00:00:00");
+today=new Date();
+timeold=(today.getTime()-BirthDay.getTime());
+sectimeold=timeold/1000
+secondsold=Math.floor(sectimeold);
+msPerDay=24*60*60*1000
+e_daysold=timeold/msPerDay
+daysold=Math.floor(e_daysold);
+e_hrsold=(e_daysold-daysold)*24;
+hrsold=Math.floor(e_hrsold);
+e_minsold=(e_hrsold-hrsold)*60;
+minsold=Math.floor((e_hrsold-hrsold)*60);
+seconds=Math.floor((e_minsold-minsold)*60);
+span_dt_dt.innerHTML='<font style=color:#afb4db>'+daysold+'</font> 天 <font style=color:#f391a9>'+hrsold+'</font> 时 <font style=color:#fdb933>'+minsold+'</font> 分 <font style=color:#a3cf62>'+seconds+'</font> 秒';
+}
